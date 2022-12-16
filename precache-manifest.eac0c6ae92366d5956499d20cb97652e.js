ec2973c40d86e3642554d3a75cb0d6a2a0e58cbb
collie <2541474139@qq.com>
2022-12-16 21:08:59 +0800

    deploy

---
+++ precache-manifest.eac0c6ae92366d5956499d20cb97652e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e483bc5d44bf7e27a907",
+    "url": "/account-website/css/app.b34be857.css"
+  },
+  {
+    "revision": "c2b995149fcd496e3460",
+    "url": "/account-website/css/chunk-vendors.93d24521.css"
+  },
+  {
+    "revision": "515ece6488471028e776963f6b0898b6",
+    "url": "/account-website/img/tick.515ece64.png"
+  },
+  {
+    "revision": "6aac8adc528303fbc8a9c5bab3e1e8e9",
+    "url": "/account-website/img/untick.6aac8adc.png"
+  },
+  {
+    "revision": "37efedf1d87fc6109be01eb0faa5a554",
+    "url": "/account-website/index.html"
+  },
+  {
+    "revision": "e483bc5d44bf7e27a907",
+    "url": "/account-website/js/app.f7129a5d.js"
+  },
+  {
+    "revision": "c2b995149fcd496e3460",
+    "url": "/account-website/js/chunk-vendors.ba03e7e3.js"
+  },
+  {
+    "revision": "62154fe6c8c988ba6d994ec9b01c90b6",
+    "url": "/account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-website/robots.txt"
+  }
+]);
